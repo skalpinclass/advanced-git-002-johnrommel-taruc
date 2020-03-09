92669fda97e76a0a7ca9a031f40040f412196331
Stephen Kalpin <skalpin@gmail.com>
2020-03-09 08:44:42 -0500

    adding unit test project

---
+++ Magenic.Cli.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magenic.Cli.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
