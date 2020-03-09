f2768526443b9c9e966559b1c30d1c240cbb91cd
Stephen Kalpin <skalpin@gmail.com>
2020-03-09 08:39:11 -0500

    adding cli application

---
+++ Magenic.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magenic.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
